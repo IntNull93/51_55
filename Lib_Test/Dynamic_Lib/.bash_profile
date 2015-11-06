b11699987aaef498c6f217ad6de4177492ee46bf
IntNull93 <woshixdss@163.com>
2015-11-06 11:12:38 +0800

    add lib_test

---
+++ Lib_Test/Dynamic_Lib/.bash_profile
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=/root/Lib_Test/Dynamic_Lib
---
+++ Lib_Test/Dynamic_Lib/test_a.c
@@ -0,0 +1,21 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  test_a.c
+ *
+ *    Description:  
+ *
+ *     Created on:  2015Äê11ÔÂ06ÈÕ 
+ *
+ *     Created by:  root
+ *
+ * =============================================================================
+ */
+
+#include "so_test.h"
+
+void test_a()
+{
+    printf("this is in test_aaaaaaaaaaaaaaaaaaaa...\n");
+}
+
